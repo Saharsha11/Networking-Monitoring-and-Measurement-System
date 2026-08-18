e9f95eb7e5c57b157892b4e280a9014ade4a4120
Saharsha Bajracharya <bajracharyas442@gmail.com>
2026-08-18 14:32:09 +0545

    Backend setup done

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,11 +131,15 @@ MAILERS = {
 }
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
 CORS_ALLOWED_ORIGINS = [
+    
     "http://localhost:5173",
 ]
